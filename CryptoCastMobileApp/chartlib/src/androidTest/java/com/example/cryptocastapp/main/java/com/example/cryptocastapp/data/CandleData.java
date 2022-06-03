54302158974d8e1a12f420f18d0288560d50814b
Berke <47689522+berkecinar@users.noreply.github.com>
2022-06-03 20:18:07 +0300

    Add files via upload

---
+++ CryptoCastMobileApp/chartlib/src/androidTest/java/com/example/cryptocastapp/main/java/com/example/cryptocastapp/data/CandleData.java
@@ -0,0 +1,23 @@
+package com.example.cryptocastapp.data;
+
+//import com.github.mikephil.charting.interfaces.datasets.ICandleDataSet;
+import com.example.cryptocastapp.interfaces.datasets.ICandleDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CandleData extends BarLineScatterCandleBubbleData<ICandleDataSet> {
+
+    public CandleData() {
+        super();
+    }
+
+    public CandleData(List<ICandleDataSet> dataSets) {
+        super(dataSets);
+    }
+
+    public CandleData(ICandleDataSet... dataSets) {
+        super(dataSets);
+    }
+}
+
---
+++ CryptoCastMobileApp/chartlib/src/androidTest/java/com/example/cryptocastapp/main/java/com/example/cryptocastapp/data/LineData.java
@@ -0,0 +1,28 @@
+package com.example.cryptocastapp.data;
+
+
+//import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.example.cryptocastapp.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Data object that encapsulates all data associated with a LineChart.
+ *
+ * @author Philipp Jahoda
+ */
+public class LineData extends BarLineScatterCandleBubbleData<ILineDataSet> {
+
+    public LineData() {
+        super();
+    }
+
+    public LineData(ILineDataSet... dataSets) {
+        super(dataSets);
+    }
+
+    public LineData(List<ILineDataSet> dataSets) {
+        super(dataSets);
+    }
+}
